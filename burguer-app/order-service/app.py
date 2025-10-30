67be8d2736878903b1fcfd29909a9e3128c65cf3
Leônidas Araújo <leonidasaraujomqm@gmail.com>
2025-10-30 15:27:23 -0300

    Refactor code formatting for consistency in app.py

--- burguer-app/order-service/app.py
+++ burguer-app/order-service/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, redirect, url_for
+import os
+
 from controllers.order_controller import order_bp
 from dotenv import load_dotenv
-import os
+from flask import Flask, redirect, url_for
 
 # Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
@@ -11,14 +12,16 @@ app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 
 # Registra o blueprint de pedidos
-app.register_blueprint(order_bp, url_prefix='/order')
+app.register_blueprint(order_bp, url_prefix="/order")
+
 
 # Redireciona a rota raiz para a lista de pedidos
-@app.route('/')
+@app.route("/")
 def index():
-    return redirect(url_for('order.list_orders'))
+    return redirect(url_for("order.list_orders"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Executa a aplicação Flask
     app.run(port=5002, debug=True)
     # O debug=True permite recarregar automaticamente a aplicação ao fazer alterações no código
