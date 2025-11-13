71aa5670d6df87c3b39c4d3f3dd349dc385a4bda
Leônidas Araújo <leonidasaraujomqm@gmail.com>
2025-11-13 17:43:28 -0300

    Add Docker configuration for MongoDB

---
+++ burguer-app/mongo/init-mongo.js
@@ -0,0 +1,8 @@
+db = db.getSiblingDB("burguer_app_db");
+
+// Cria usuário do Mongo para o app
+db.createUser({
+  user: "appuser",
+  pwd: "apppass",
+  roles: [{ role: "readWrite", db: "burguer_app_db" }]
+});
